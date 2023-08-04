e96cb0e8bf4212e8246f3ea84784f7588b649d38
Maryna Boieva <yamirovoezlo@gmail.com>
2023-08-04 05:24:45 +0200

    Add basic structure

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,62 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
+  Form,
   Outlet,
-  Scripts,
-  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+  useSearchParams,
 } from "@remix-run/react";
+import Document from "./components/Document";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
+  const [params] = useSearchParams();
+
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <Document>
+      <Form method="get">
+        <input
+          type="text"
+          name="city"
+          defaultValue={params.get("city") || ""}
+        />
+        <button type="submit">Search</button>
+      </Form>
+      <Outlet />
+    </Document>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  const renderDetails = () => {
+    if (isRouteErrorResponse(error)) {
+      return (
+        <div>
+          <h1>
+            {error.status} {error.statusText}
+          </h1>
+          <p>{error.data}</p>
+        </div>
+      );
+    } else if (error instanceof Error) {
+      return (
+        <div>
+          <h1>Error</h1>
+          <p>{error.message}</p>
+          <p>The stack trace is:</p>
+          <pre>{error.stack}</pre>
+        </div>
+      );
+    } else {
+      return <h1>Unknown Error</h1>;
+    }
+  };
+
+  return <Document title="Error">{renderDetails()}</Document>;
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,51 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunction } from "@remix-run/node";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import axios from "axios";
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const city = new URLSearchParams(url.search).get("city");
+
+  const apiUrl = `${process.env.BACKEND_API}/weather?city=${
+    city || "Copenhagen"
+  }`;
+
+  try {
+    const { data } = await axios.get(apiUrl);
+    return json({ data });
+  } catch (err) {
+    throw new Response("Sorry, we found no match for your location", {
+      status: 404,
+    });
+  }
 };
 
 export default function Index() {
+  const { data } = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h2>weather in {data.city}</h2>
+      <p>{data.temp}</p>
+      <p>{data.main}</p>
+    </>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <p>{error.data}</p>
+      </div>
+    );
+  }
+
+  throw error;
 }
